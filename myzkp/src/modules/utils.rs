8c684fc7b211f79909286ed1f9bf0df9a922abb8
Koukyosyumei <koukyosyumei@hotmail.com>
2024-12-20 11:16:50 -0500

    remove unnecessary clones

--- myzkp/src/modules/utils.rs
+++ myzkp/src/modules/utils.rs
@@ -1,9 +1,13 @@
 use num_traits::{One, Zero};
-use std::ops::{Div, Rem, Sub};
+use std::ops::{Div, Mul, Rem, Sub};
 
 pub fn extended_euclidean<F>(a: F, b: F) -> (F, F, F)
 where
     F: Clone + PartialEq + Sub<Output = F> + Div<Output = F> + Rem<Output = F> + Zero + One,
+    for<'a> &'a F: Sub<&'a F, Output = F>
+        + Div<&'a F, Output = F>
+        + Rem<&'a F, Output = F>
+        + Mul<&'a F, Output = F>,
 {
     let mut r0 = a;
     let mut r1 = b;
@@ -13,14 +17,14 @@ where
     let mut t1 = F::one();
 
     while !r1.is_zero() {
-        let q = r0.clone() / r1.clone();
-        let r = r0.clone() % r1.clone();
+        let q = &r0 / &r1;
+        let r = &r0 % &r1;
         r0 = r1;
         r1 = r;
-        let new_s = s0.clone() - q.clone() * s1.clone();
+        let new_s = s0 - &q * &s1;
         s0 = s1;
         s1 = new_s;
-        let new_t = t0.clone() - q * t1.clone();
+        let new_t = t0.clone() - &q * &t1;
         t0 = t1;
         t1 = new_t;
     }
