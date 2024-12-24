ae9c1844c3d050300b051e03ef7a973e5fa12823
Koukyosyumei <koukyosyumei@hotmail.com>
2024-12-24 05:10:18 -0500

    update

--- myzkp/src/modules/mod.rs
+++ myzkp/src/modules/mod.rs
@@ -1,11 +1,11 @@
 pub mod bn128;
 pub mod curve;
-pub mod educational_protocols;
 pub mod efield;
 pub mod field;
 pub mod polynomial;
 pub mod qap;
 pub mod r1cs;
 pub mod ring;
+pub mod single_polynomial;
 pub mod utils;
 // pub mod snark;
