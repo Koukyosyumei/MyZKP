bf33376eb23cab17a6232682c00eafc6e4577372
Koukyosyumei <koukyosyumei@hotmail.com>
2024-12-25 21:05:25 -0500

    update

--- myzkp/src/modules/zksnark/mod.rs
+++ myzkp/src/modules/zksnark/mod.rs
@@ -1,3 +1,4 @@
 pub mod naive;
 pub mod non_interchangibility;
+pub mod utils;
 pub mod variable_consistency;
--- myzkp/src/modules/zksnark/naive.rs
+++ myzkp/src/modules/zksnark/naive.rs
@@ -9,6 +9,10 @@ use crate::modules::field::Field;
 use crate::modules::polynomial::Polynomial;
 use crate::modules::qap::QAP;
 use crate::modules::ring::Ring;
+use crate::modules::zksnark::utils::{
+    accumulate_curve_points, accumulate_polynomials, generate_alpha_challenge_vec,
+    generate_challenge_vec, generate_s_powers, get_h,
+};
 
 #[derive(Debug, Clone)]
 pub struct ProofKey {
@@ -38,69 +42,6 @@ pub struct Proof {
     g1_r_prime: G1Point,
     g1_o_prime: G1Point,
     g1_h: G1Point,
-}
-
-pub fn generate_challenge_vec<F1: Field, F2: Field, E: EllipticCurve>(
-    point: &EllipticCurvePoint<F1, E>,
-    poly_vec: &[Polynomial<F2>],
-    s: &F2,
-) -> Vec<EllipticCurvePoint<F1, E>> {
-    poly_vec
-        .iter()
-        .map(|poly| point.mul_ref(poly.eval(s).sanitize().get_value()))
-        .collect()
-}
-
-pub fn generate_alpha_challenge_vec<F1: Field, F2: Field, E: EllipticCurve>(
-    point: &EllipticCurvePoint<F1, E>,
-    poly_vec: &[Polynomial<F2>],
-    s: &F2,
-    alpha: &F2,
-) -> Vec<EllipticCurvePoint<F1, E>> {
-    poly_vec
-        .iter()
-        .map(|poly| point.mul_ref((alpha.mul_ref(&poly.eval(s).sanitize())).get_value()))
-        .collect()
-}
-
-pub fn generate_s_powers<F1: Field, F2: Field, E: EllipticCurve>(
-    point: &EllipticCurvePoint<F1, E>,
-    s: &F2,
-    m: usize,
-) -> Vec<EllipticCurvePoint<F1, E>> {
-    let mut powers = Vec::with_capacity(m + 1);
-    let mut current = F2::one();
-    for _ in 0..=m {
-        powers.push(point.mul_ref(current.get_value()));
-        current = current * s.clone();
-    }
-    powers
-}
-
-fn accumulate_curve_points<F1: Field, F2: Field, E: EllipticCurve>(
-    g_vec: &[EllipticCurvePoint<F1, E>],
-    assignment: &[F2],
-) -> EllipticCurvePoint<F1, E> {
-    g_vec.iter().zip(assignment.iter()).fold(
-        EllipticCurvePoint::<F1, E>::point_at_infinity(),
-        |acc, (g, &ref a)| acc + g.mul_ref(a.get_value()),
-    )
-}
-
-fn accumulate_polynomials<F: Field>(poly_vec: &[Polynomial<F>], assignment: &[F]) -> Polynomial<F> {
-    poly_vec
-        .iter()
-        .zip(assignment.iter())
-        .fold(Polynomial::<F>::zero(), |acc, (poly, &ref a)| {
-            acc + poly.clone() * a.clone()
-        })
-}
-
-fn get_h<F: Field>(qap: &QAP<F>, assignment: &Vec<F>) -> Polynomial<F> {
-    let ell = accumulate_polynomials(&qap.ell_i_vec, assignment);
-    let r = accumulate_polynomials(&qap.r_i_vec, assignment);
-    let o = accumulate_polynomials(&qap.o_i_vec, assignment);
-    (ell * r - o) / qap.t.clone()
 }
 
 pub fn setup(g1: &G1Point, g2: &G2Point, qap: &QAP<FqOrder>) -> (ProofKey, VerificationKey) {
