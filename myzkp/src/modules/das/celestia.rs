a3361e92d352dfbdcf68c72f7bef39cf2cd113fb
Koukyosyumei <koukyosyumei@hotmail.com>
2025-03-27 12:28:36 -0400

    update

--- myzkp/src/modules/das/celestia.rs
+++ myzkp/src/modules/das/celestia.rs
@@ -6,11 +6,12 @@ pub type EncodedDataCelestia = Vec<Vec<Vec<u8>>>;
 pub struct CommitmentCelestia {
     pub row_roots: Vec<Vec<u8>>,
     pub col_roots: Vec<Vec<u8>>,
+    pub data_root: Vec<u8>,
 }
 
 pub struct ProofCelestia {
-    pub row_proof: Vec<Vec<u8>>,
-    pub col_proof: Vec<Vec<u8>>,
+    pub proof: Vec<Vec<u8>>,
+    pub is_left: bool,
 }
 
 pub struct Celestia;
@@ -42,29 +43,39 @@ impl Celestia {
             })
             .collect();
 
+        let mut all_roots = row_roots.clone();
+        all_roots.extend(col_roots.iter().cloned());
+        let data_root = Merkle::commit(&all_roots);
+
         (
             reshaped,
             CommitmentCelestia {
                 row_roots,
                 col_roots,
+                data_root,
             },
         )
     }
 
-    pub fn sample(row_id: usize, col_id: usize, encoded: &EncodedDataCelestia) -> ProofCelestia {
-        let row_proof = Merkle::open(col_id, &encoded[row_id]);
-        let col_proof = Merkle::open(
-            row_id,
-            &encoded
-                .iter()
-                .map(|row| row[col_id].clone())
-                .collect::<Vec<_>>(),
-        );
+    pub fn sample(
+        row_id: usize,
+        col_id: usize,
+        encoded: &EncodedDataCelestia,
+        is_left: bool,
+    ) -> ProofCelestia {
+        let proof = if is_left {
+            Merkle::open(col_id, &encoded[row_id])
+        } else {
+            Merkle::open(
+                row_id,
+                &encoded
+                    .iter()
+                    .map(|row| row[col_id].clone())
+                    .collect::<Vec<_>>(),
+            )
+        };
 
-        ProofCelestia {
-            row_proof,
-            col_proof,
-        }
+        ProofCelestia { proof, is_left }
     }
 
     pub fn verify(
@@ -74,17 +85,11 @@ impl Celestia {
         commitment: &CommitmentCelestia,
         proof: &ProofCelestia,
     ) -> bool {
-        Merkle::verify(
-            &commitment.row_roots[row_id],
-            col_id,
-            &proof.row_proof,
-            chunk,
-        ) && Merkle::verify(
-            &commitment.col_roots[col_id],
-            row_id,
-            &proof.col_proof,
-            chunk,
-        )
+        if proof.is_left {
+            Merkle::verify(&commitment.row_roots[row_id], col_id, &proof.proof, chunk)
+        } else {
+            Merkle::verify(&commitment.col_roots[col_id], row_id, &proof.proof, chunk)
+        }
     }
 }
 
@@ -96,28 +101,51 @@ mod tests {
     fn test_celestia_no_error() {
         let data = vec![1, 2, 3, 4];
         let (encoded, commitment) = Celestia::encode(4, 4, &data);
-        let proof_00 = Celestia::sample(0, 0, &encoded);
+        let proof_00_left = Celestia::sample(0, 0, &encoded, true);
         assert!(Celestia::verify(
             0,
             0,
             &encoded[0][0],
             &commitment,
-            &proof_00
+            &proof_00_left
         ));
-        let proof_01 = Celestia::sample(0, 1, &encoded);
+        let proof_00_right = Celestia::sample(0, 0, &encoded, false);
+        assert!(Celestia::verify(
+            0,
+            0,
+            &encoded[0][0],
+            &commitment,
+            &proof_00_right
+        ));
+        let proof_01_left = Celestia::sample(0, 1, &encoded, true);
         assert!(Celestia::verify(
             0,
             1,
             &encoded[0][1],
             &commitment,
-            &proof_01
+            &proof_01_left
+        ));
+        let proof_01_right = Celestia::sample(0, 1, &encoded, false);
+        assert!(Celestia::verify(
+            0,
+            1,
+            &encoded[0][1],
+            &commitment,
+            &proof_01_right
         ));
         assert!(!Celestia::verify(
             0,
             1,
             &encoded[1][0],
             &commitment,
-            &proof_01
+            &proof_01_left
+        ));
+        assert!(!Celestia::verify(
+            0,
+            1,
+            &encoded[1][0],
+            &commitment,
+            &proof_01_right
         ));
     }
 }
