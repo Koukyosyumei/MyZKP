909c29100be485257afe07a8f4b0019c578e5b6d
Koukyosyumei <koukyosyumei@hotmail.com>
2025-04-29 09:55:48 -0400

    update

--- myzkp/src/modules/zkstark/rescueprime.rs
+++ myzkp/src/modules/zkstark/rescueprime.rs
@@ -12,7 +12,7 @@ use crate::modules::algebra::polynomial::Polynomial;
 use crate::modules::algebra::ring::Ring;
 use crate::modules::zkstark::fiat_shamir::FiatShamirTransformer;
 use crate::modules::zkstark::fri::FRI;
-use crate::modules::zkstark::stark::Stark;
+use crate::modules::zkstark::stark::{Boundary, Stark, TransitionConstraints};
 
 pub struct RescuePrime<M: ModulusValue> {
     pub p: usize,
@@ -80,5 +80,79 @@ impl<M: ModulusValue> RescuePrime<M> {
         }
 
         state[0].clone()
+    }
+
+    fn round_constants_polynomials(
+        &self,
+        omicron: &FiniteFieldElement<M>,
+    ) -> (
+        Vec<MPolynomial<FiniteFieldElement<M>>>,
+        Vec<MPolynomial<FiniteFieldElement<M>>>,
+    ) {
+        let mut first_step_constants = Vec::new();
+        for i in 0..self.m {
+            let domain: Vec<_> = (0..self.n).map(|r| omicron.pow(r)).collect();
+            let values: Vec<_> = (0..self.n)
+                .map(|r| self.round_constants[2 * r * self.m + i].clone())
+                .collect();
+            let univariate = Polynomial::interpolate(&domain, &values);
+            let multivariate = MPolynomial::lift(&univariate, 0);
+            first_step_constants.push(multivariate);
+        }
+
+        let mut second_step_constants = Vec::new();
+        for i in 0..self.m {
+            let domain: Vec<_> = (0..self.n).map(|r| omicron.pow(r)).collect();
+            let values: Vec<_> = (0..self.n)
+                .map(|r| self.round_constants[2 * r * self.m + self.m + i].clone())
+                .collect();
+            let univariate = Polynomial::interpolate(&domain, &values);
+            let multivariate = MPolynomial::lift(&univariate, 0);
+            second_step_constants.push(multivariate);
+        }
+
+        (first_step_constants, second_step_constants)
+    }
+
+    pub fn transition_constraints(
+        &self,
+        omicron: FiniteFieldElement<M>,
+    ) -> TransitionConstraints<M> {
+        let (first_step_constants, second_step_constants) =
+            self.round_constants_polynomials(&omicron);
+
+        let variables = MPolynomial::<FiniteFieldElement<M>>::variables(1 + 2 * self.m);
+        //let cycle_index = &variables[0];
+        let previous_state = &variables[1..(1 + self.m)];
+        let next_state = &variables[(1 + self.m)..(1 + 2 * self.m)];
+
+        let mut air = Vec::new();
+        for i in 0..self.m {
+            let mut lhs = MPolynomial::constant(FiniteFieldElement::<M>::zero());
+            for k in 0..self.m {
+                lhs = lhs
+                    + MPolynomial::constant(self.mds[i][k].clone())
+                        * (previous_state[k].pow(self.alpha));
+            }
+            lhs = lhs + first_step_constants[i].clone();
+
+            let mut rhs = MPolynomial::constant(FiniteFieldElement::<M>::zero());
+            for k in 0..self.m {
+                rhs = rhs
+                    + MPolynomial::constant(self.mdsinv[i][k].clone())
+                        * (&next_state[k] - &second_step_constants[k]);
+            }
+            rhs = rhs.pow(self.alpha);
+            air.push(lhs - rhs);
+        }
+
+        air
+    }
+
+    pub fn boundary_constraints(&self, output_element: FiniteFieldElement<M>) -> Boundary<M> {
+        let mut constraints = Vec::new();
+        constraints.push((0, 1, FiniteFieldElement::<M>::zero()));
+        constraints.push((self.n, 0, output_element));
+        constraints
     }
 }
