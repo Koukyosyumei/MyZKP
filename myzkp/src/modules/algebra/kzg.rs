30c74bf317bdb972488e7f40bf8e11b50cbd79b2
Koukyosyumei <koukyosyumei@hotmail.com>
2025-08-30 09:57:14 -0400

    update

--- myzkp/src/modules/algebra/kzg.rs
+++ myzkp/src/modules/algebra/kzg.rs
@@ -1,3 +1,5 @@
+use std::ops::Sub;
+
 use num_traits::{One, Zero};
 
 use crate::modules::algebra::curve::bn128::{optimal_ate_pairing, FqOrder, G1Point, G2Point};
@@ -13,6 +15,11 @@ pub type CommitmentKZG = G1Point;
 
 pub struct ProofKZG {
     pub y: FqOrder,
+    pub w: G1Point,
+}
+
+pub struct BatchProofKZG {
+    pub ys: Vec<FqOrder>,
     pub w: G1Point,
 }
 
@@ -57,10 +64,26 @@ pub fn open_kzg(p: &Polynomial<FqOrder>, z: &FqOrder, pk: &PublicKeyKZG) -> Proo
     let y_poly = Polynomial {
         coef: (&[y.clone()]).to_vec(),
     };
-
     let q = (p - &y_poly) / Polynomial::<FqOrder>::from_monomials(&[z.clone()]);
+
     ProofKZG {
         y: y,
+        w: q.eval_with_powers_on_curve(&pk.powers_1),
+    }
+}
+
+pub fn batch_open_kzg(
+    p: &Polynomial<FqOrder>,
+    zs: &Vec<FqOrder>,
+    pk: &PublicKeyKZG,
+) -> BatchProofKZG {
+    let ys = zs.iter().map(|z| p.eval(z)).collect::<Vec<_>>();
+    let ip = Polynomial::interpolate(zs, &ys);
+    let denominator = Polynomial::from_monomials(zs);
+    let q = (p - &ip) / denominator;
+
+    BatchProofKZG {
+        ys: ys,
         w: q.eval_with_powers_on_curve(&pk.powers_1),
     }
 }
@@ -77,6 +100,23 @@ pub fn verify_kzg(z: &FqOrder, c: &CommitmentKZG, proof: &ProofKZG, pk: &PublicK
     let e3 = optimal_ate_pairing(&c, &g2);
 
     e3 == e1 * (e2.pow(proof.y.get_value()))
+}
+
+pub fn batch_verify_kzg(
+    zs: &Vec<FqOrder>,
+    c: &CommitmentKZG,
+    proof: &BatchProofKZG,
+    pk: &PublicKeyKZG,
+) -> bool {
+    let g2 = &pk.powers_2[0];
+    let ip = Polynomial::interpolate(zs, &proof.ys);
+    let denominator = Polynomial::from_monomials(zs);
+    let g1_ip = ip.eval_with_powers_on_curve(&pk.powers_1);
+    let g2_z = denominator.eval_with_powers_on_curve(&pk.powers_2);
+
+    let e1 = optimal_ate_pairing(&proof.w, &g2_z);
+    let e2 = optimal_ate_pairing(&(c.clone() - g1_ip), g2);
+    e1 == e2
 }
 
 pub fn prove_degree_bound(
@@ -133,6 +173,36 @@ mod tests {
         // Verification
         let flag = verify_kzg(&z, &c, &proof, &pk);
         assert!(flag);
+    }
+
+    #[test]
+    fn test_batch_kzg() {
+        let g1 = BN128::generator_g1();
+        let g2 = BN128::generator_g2();
+
+        let p = Polynomial::from_monomials(&[
+            FqOrder::from_value(-1),
+            FqOrder::from_value(-2),
+            FqOrder::from_value(-3),
+        ]);
+
+        let pk = setup_kzg_with_full_g2(&g1, &g2, 3);
+
+        // Commitment
+        let c = commit_kzg(&p, &pk);
+
+        // Proof
+        let zs = vec![FqOrder::from_value(5), FqOrder::from_value(7)];
+        let mut proof = batch_open_kzg(&p, &zs, &pk);
+
+        // Verification
+        let flag = batch_verify_kzg(&zs, &c, &proof, &pk);
+        assert!(flag);
+
+        // Verification
+        proof.ys[0] += FqOrder::one();
+        let flag = batch_verify_kzg(&zs, &c, &proof, &pk);
+        assert!(!flag);
     }
 
     #[test]
