d0ca443d461c164a4e8680b099aab853fb1a6c20
Koukyosyumei <koukyosyumei@hotmail.com>
2025-10-13 14:31:09 +0000

    update

--- myzkp/examples/sumcheck_cuda/src/main.rs
+++ myzkp/examples/sumcheck_cuda/src/main.rs
@@ -16,20 +16,43 @@ fn f_to_bytes(x: &F) -> Vec<u8> {
     digits.iter().flat_map(|d| d.to_le_bytes()).collect()
 }
 
+fn vec_f_to_bytes(xs: &Vec<F>) -> Vec<u8> {
+    xs.iter().flat_map(|x| f_to_bytes(x)).collect()
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    //let contents = fs::read_to_string("../../src/modules/algebra/cuda/kernels/sumcheck.hpp")?;
-    let ptx = Ptx::from_file("../../src/modules/algebra/cuda/kernels/sumcheck.cu"); //.unwrap();
+    // let ptx = compile_ptx("").unwrap();
+    let ptx = Ptx::from_file("../../src/modules/algebra/cuda/kernels/sumcheck.ptx"); //.unwrap();
 
     let a = F::one();
+    let b = F::one();
+    let x = vec![a, b];
 
     let ctx = cudarc::driver::CudaContext::new(0)?;
     let stream = ctx.default_stream();
 
-    // copy a rust slice to the device
-    let inp = stream.memcpy_stod(&[1.0f32; 100])?;
+    let module = ctx.load_module(ptx)?;
+    let sum_kernel = module.load_function("sum")?;
 
+    
+    // copy a rust slice to the device
+    let x_bytes = vec_f_to_bytes(&x);
+    // println!(":{}")
+    let inp = stream.memcpy_stod(&x_bytes)?;
+    let mut out = stream.alloc_zeros::<u8>(32)?;
+    
+
+    let mut builder = stream.launch_builder(&sum_kernel);
+    builder.arg(&inp);
+    builder.arg(&mut out);
+    builder.arg(&2usize);
+    builder.arg(&0usize);
+    unsafe { builder.launch(LaunchConfig::for_num_elems(2)) }?;
+    let out_host: Vec<u8> = stream.memcpy_dtov(&out)?;
+    println!("out_host: {:?}", out_host);
+
+    /*
     // or allocate directly
-    let mut out = stream.alloc_zeros::<f32>(100)?;
 
     let ptx = cudarc::nvrtc::compile_ptx("
     extern \"C\" __global__ void sin_kernel(float *out, const float *inp, const size_t numel) {
@@ -51,6 +74,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let out_host: Vec<f32> = stream.memcpy_dtov(&out)?;
     assert_eq!(out_host, [1.0; 100].map(f32::sin));
+    */
 
     Ok(())
 }
