2e6e55421b386b3df6d257f44874697526bd2f53
Koukyosyumei <koukyosyumei@hotmail.com>
2025-10-13 13:37:06 +0000

    update

--- myzkp/examples/sumcheck_cuda/src/main.rs
+++ myzkp/examples/sumcheck_cuda/src/main.rs
@@ -1,8 +1,43 @@
+use cudarc;
+use cudarc::driver::PushKernelArg;
+use cudarc::driver::LaunchConfig;
 use num_traits::identities::One;
 use myzkp::modules::algebra::field::{FiniteFieldElement, ModEIP197};
 
 type F = FiniteFieldElement<ModEIP197>;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let a = F::one();
+
+    let ctx = cudarc::driver::CudaContext::new(0)?;
+    let stream = ctx.default_stream();
+
+    // copy a rust slice to the device
+    let inp = stream.memcpy_stod(&[1.0f32; 100])?;
+
+    // or allocate directly
+    let mut out = stream.alloc_zeros::<f32>(100)?;
+
+    let ptx = cudarc::nvrtc::compile_ptx("
+    extern \"C\" __global__ void sin_kernel(float *out, const float *inp, const size_t numel) {
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < numel) {
+        out[i] = sin(inp[i]);
+    }
+    }")?;
+
+    // Dynamically load it into the device
+    let module = ctx.load_module(ptx)?;
+    let sin_kernel = module.load_function("sin_kernel")?;
+
+    let mut builder = stream.launch_builder(&sin_kernel);
+    builder.arg(&mut out);
+    builder.arg(&inp);
+    builder.arg(&100usize);
+    unsafe { builder.launch(LaunchConfig::for_num_elems(100)) }?;
+
+    let out_host: Vec<f32> = stream.memcpy_dtov(&out)?;
+    assert_eq!(out_host, [1.0; 100].map(f32::sin));
+
+    Ok(())
 }
