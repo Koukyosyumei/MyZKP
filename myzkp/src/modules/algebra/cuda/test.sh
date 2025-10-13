1e06a6777e77b78fd78cd88f878bd88167ba0b55
Koukyosyumei <koukyosyumei@hotmail.com>
2025-10-13 13:18:21 +0000

    update

--- myzkp/src/modules/algebra/cuda/test.sh
+++ myzkp/src/modules/algebra/cuda/test.sh
@@ -1,4 +1,8 @@
-nvcc -std=c++17 test_fr.cu -o test_fr
-nvcc -std=c++17 test_sumcheck.cu -o test_sumcheck
-./test_fr
-./test_sumcheck
+mkdir build
+
+nvcc -std=c++17 test_fr.cu -o build/test_fr
+nvcc -std=c++17 test_sumcheck.cu -o build/test_sumcheck
+./build/test_fr
+./build/test_sumcheck
+
+rm -rf build
