2395d591474c4b7e2604141fa6b0864d702c1893
Koukyosyumei <koukyosyumei@hotmail.com>
2025-10-14 01:29:26 +0000

    update

--- myzkp/examples/sumcheck_cuda/src/main.rs
+++ myzkp/examples/sumcheck_cuda/src/main.rs
@@ -140,7 +140,7 @@ impl<'a> SumCheckProver<'a> {
         }
     }
 
-    pub fn prove(&self, num_variables: usize, max_degree: usize, polynomial_product: MPolynomial<F>, polynomial_factors: &[MPolynomial<F>]) -> Result<FiatShamirTransformer, Box<dyn std::error::Error>> {
+    pub fn prove(&self, num_variables: usize, max_degree: usize, polynomial_factors: &[MPolynomial<F>]) -> Result<FiatShamirTransformer, Box<dyn std::error::Error>> {
         let num_factors = polynomial_factors.len();
         let mut evaluation_table = vec![];
         for f in polynomial_factors {
@@ -152,11 +152,16 @@ impl<'a> SumCheckProver<'a> {
             bincode::serialize(&max_degree).expect("Serialization failed")
         ]);
         transcript.push(&vec![
-            bincode::serialize(&num_variables).expect("Serialization failed")
+            bincode::serialize(&num_factors).expect("Serialization failed")
         ]);
         transcript.push(&vec![
-            bincode::serialize(&polynomial_product).expect("Serialization failed")
+            bincode::serialize(&num_variables).expect("Serialization failed")
         ]);
+        for i in 0..num_factors {
+            transcript.push(&vec![
+                bincode::serialize(&polynomial_factors[i]).expect("Serialization failed")
+            ]);
+        }
 
         let mut round_polynomials = Vec::with_capacity(num_variables);
         let mut challenges = Vec::with_capacity(num_variables);
@@ -319,12 +324,13 @@ impl SumCheckVerifier {
     pub fn verify(
         max_degree: usize,
         num_vars: usize,
-        polynomial_product: &MPolynomial<F>,
+        polynomial_factors: &[MPolynomial<F>],
         claimed_sum: F,
         transcript: &mut FiatShamirTransformer,
     ) -> Result<bool, Box<dyn std::error::Error>> {
         //let num_variables = polynomial_factors.iter().map(|p| p.get_num_vars()).max().unwrap_or(0);
 
+        let polynomial_product = polynomial_factors.iter().skip(1).fold(polynomial_factors[0].clone(), |acc, p| &acc * p);
         let s_eval_point: Vec<F> = (0..(max_degree + 1)).map(|d| F::from_value(d)).collect();
 
         let recovered_max_degree = transcript.pull();
@@ -332,16 +338,16 @@ impl SumCheckVerifier {
             vec![bincode::serialize(&max_degree).expect("Serialization failed")],
             recovered_max_degree
         );
-        let recovered_num_vars = transcript.pull();
-        assert_eq!(
-            vec![bincode::serialize(&num_vars).expect("Serialization failed")],
-            recovered_num_vars
-        );
-        let recovered_g = transcript.pull();
-        assert_eq!(
-            vec![bincode::serialize(&polynomial_product).expect("Serialization failed")],
-            recovered_g
-        );
+        let recovered_num_factors = bincode::deserialize::<usize>(&transcript.pull()[0])?;
+        assert_eq!(recovered_num_factors, polynomial_factors.len());
+        let recovered_num_vars = bincode::deserialize::<usize>(&transcript.pull()[0])?;
+        assert_eq!(recovered_num_vars, num_vars);
+        for i in 0..recovered_num_factors {
+            assert_eq!(
+                vec![bincode::serialize(&polynomial_factors[i]).expect("Serialization failed")],
+                transcript.pull()
+            );
+        }
 
         let mut s_polys = vec![];
         let mut challenges: Vec<F> = vec![];
@@ -432,8 +438,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &cuda_backend,
     );
 
-    let mut transcript = prover.prove(num_vars, max_degree, g.clone(), &factors)?;
-    let is_valid = SumCheckVerifier::verify(max_degree, num_vars, &g, h, &mut transcript)?;
+    let mut transcript = prover.prove(num_vars, max_degree, &factors)?;
+    let is_valid = SumCheckVerifier::verify(max_degree, num_vars, &factors, h, &mut transcript)?;
     assert!(is_valid);
 
     println!("success");
